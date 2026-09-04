7b5e1e91904c23ea273675edafb7f4824d012733
karianyu <mibangoe041@gmail.com>
2026-09-04 20:49:32 +0300

    db port for backup tables

--- agent/site.py
+++ agent/site.py
@@ -49,6 +49,7 @@ class Site(Base):
         self.user = self.config["db_name"]
         self.password = self.config["db_password"]
         self.host = self.config.get("db_host", self.bench.host)
+        self.db_port = 3306
 
     def bench_execute(self, command, input=None):
         return self.bench.docker_execute(f"bench --site {self.name} {command}", input=input)
